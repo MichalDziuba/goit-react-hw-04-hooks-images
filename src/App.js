16fa5deb17792d8591b0bcb2093e199ada501b44
Michał <michaldziuba.praca@gmail.com>
2022-03-09 21:26:35 +0100

    Nie działa :/

--- src/App.js
+++ src/App.js
@@ -1,65 +1,63 @@
 import "./App.css";
-import { React, Component } from "react";
+import { React, Component, useState, useEffect, useRef } from "react";
 import Searchbar from "./Components/SearchBar/Searchbar";
 import ImageGallery from "./Components/imageGallery/ImageGallery";
 // import GalleryItem from "./Components/ImageGalleryItem/ImageGalleryItem";
 import axios from "axios";
 import Button from "./Components/LoadMoreBtn/LoadMoreBtn";
 import Loader from "./Components/Loader/Loader";
-class App extends Component {
-  state = {
-    keyword: "",
-    images: [],
-    isLoading: false,
-    error: "",
-    page: 1,
-    totalPages: 0,
-  };
-  searching = async (e) => {
+function App() {
+  const [keyword, setKeyword] = useState("");
+  const [images, getImages] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, getError] = useState("");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  function searching(e) {
     e.preventDefault();
     const form = e.target;
     const input = document.querySelector("input").value;
-    // console.log(input);
-    this.setState({ keyword: input });
+    setKeyword(input);
     form.reset();
-  };
-  initialFetch = async (keyword,page) => {
+  }
+  async function initialFetch (keyworda, pagea) {
     axios.defaults.baseURL = "https://pixabay.com/api/";
     const APIKEY = "23677449-ed03e132ebac9ff9282502f83";
-
-    keyword = this.state.keyword;
-   page = this.state.page;
+//nie wiedziałem jak użyć nazwy dlatego a na końcu
+    keyworda = keyword;
+    pagea = page;
     try {
       const response = await axios.get(
-        `?key=${APIKEY}&q=${keyword}&image_type=photo&orientation=horizontal&per_page=12&page=${page}`
+        `?key=${APIKEY}&q=${keyworda}&image_type=photo&orientation=horizontal&per_page=12&page=${pagea}`
       );
       if (page === 1) {
-        this.setState({
-          images: response.data.hits,
-          totalPages: response.data.totalHits,
-          isLoading: false,
-        });
+        
+          getImages(response.data.hits)
+          setTotalPages(response.data.totalHits)
+          setLoading(false)
+       
       }
-      // if(){}
+      
       else {
-        this.setState((prevState) => ({
-          images: [...prevState.images, ...response.data.hits],
-          totalPages: response.data.totalHits,
-        }));
-        // console.log(this.state);
+        getImages((prevState) => ({
+          ...prevState,
+          ...response.data.hits
+        }))
+        setTotalPages(response.data.totalHits)
       }
 
       // console.log(this.state);
     } catch (error) {
-      this.setState({ error: error.message });
+     
+      getError(error.message)
     } finally {
-      this.setState({ isLoading: false });
+      setLoading(false)
     }
   };
-  loadMore = async () => {
-    this.setState((prevState) => ({
-      page: prevState.page + 1,
-    }));
+  
+  function loadMore() {
+    setPage(page+1)
     setTimeout(() => {
       window.scrollBy({
         top: 560,
@@ -67,47 +65,150 @@ class App extends Component {
       });
     }, 300);
   };
-  async componentDidMount() {
-    this.setState({ isLoading: true });
-    this.initialFetch();
-  }
-  async componentDidUpdate(previousProps, prevState) {
-    if (prevState.page !== this.state.page) {
+  
+   useEffect(() => {
+     setLoading(true);
+     initialFetch();
+   })
+ 
+  const prevStateKeyword = useRef();
+  const prevStatePage = useRef();
+  useEffect(() => {
+    prevStateKeyword.current = keyword;
+    prevStatePage.current = page;
+    if (prevStatePage.current !== page) {
+      initialFetch()
+    }
+    if (prevStateKeyword.current !== keyword) {
+      setLoading(true);
+      getImages([]);
+      initialFetch();
+    }
+  })
+
+  useEffect(() => {
+    setKeyword("");
+    setPage(1)
+  }, [])
+  return (
+    <div>
+      <Searchbar searching={searching} />
+
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ImageGallery items={images} />
+      )}
+
+      {/* {images.length < totalPages && ( */}
+        <Button more={loadMore} />
       
-      this.initialFetch();
-    }
-    if (prevState.keyword !== this.state.keyword) {
-      this.setState({
-        isLoading: true,
-        images:[]
-      });
-      this.initialFetch();
-    }
-  }
-  componentWillUnmount() {
-    this.setState({
-      keyword: "",
-      page: 1
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <Searchbar searching={this.searching} />
-
-        {this.state.isLoading ? (
-          <Loader />
-        ) : (
-          <ImageGallery items={this.state.images} />
-        )}
-
-        {this.state.images.length < this.state.totalPages && (
-          <Button more={this.loadMore} />
-        )}
-      </div>
-    );
-  }
+      {/* )} */}
+    </div>
+  );
 }
+
+// class App extends Component {
+//   state = {
+//     keyword: "",
+//     images: [],
+//     isLoading: false,
+//     error: "",
+//     page: 1,
+//     totalPages: 0,
+//   };
+//   searching = async (e) => {
+//     e.preventDefault();
+//     const form = e.target;
+//     const input = document.querySelector("input").value;
+//     // console.log(input);
+//     this.setState({ keyword: input });
+//     form.reset();
+//   };
+  // initialFetch = async (keyword, page) => {
+  //   axios.defaults.baseURL = "https://pixabay.com/api/";
+  //   const APIKEY = "23677449-ed03e132ebac9ff9282502f83";
+
+  //   keyword = this.state.keyword;
+  //   page = this.state.page;
+  //   try {
+  //     const response = await axios.get(
+  //       `?key=${APIKEY}&q=${keyword}&image_type=photo&orientation=horizontal&per_page=12&page=${page}`
+  //     );
+  //     if (page === 1) {
+  //       this.setState({
+  //         images: response.data.hits,
+  //         totalPages: response.data.totalHits,
+  //         isLoading: false,
+  //       });
+  //     }
+  //     // if(){}
+  //     else {
+  //       this.setState((prevState) => ({
+  //         images: [...prevState.images, ...response.data.hits],
+  //         totalPages: response.data.totalHits,
+  //       }));
+  //       // console.log(this.state);
+  //     }
+
+  //     // console.log(this.state);
+  //   } catch (error) {
+  //     this.setState({ error: error.message });
+  //   } finally {
+  //     this.setState({ isLoading: false });
+  //   }
+  // };
+//   loadMore = async () => {
+//     this.setState((prevState) => ({
+//       page: prevState.page + 1,
+//     }));
+//     setTimeout(() => {
+//       window.scrollBy({
+//         top: 560,
+//         behavior: "smooth",
+//       });
+//     }, 300);
+//   };
+//   async componentDidMount() {
+//     this.setState({ isLoading: true });
+//     this.initialFetch();
+//   }
+//   async componentDidUpdate(previousProps, prevState) {
+//     if (prevState.page !== this.state.page) {
+//       this.initialFetch();
+//     }
+//     if (prevState.keyword !== this.state.keyword) {
+//       this.setState({
+//         isLoading: true,
+//         images: [],
+//       });
+//       this.initialFetch();
+//     }
+//   }
+//   componentWillUnmount() {
+//     this.setState({
+//       keyword: "",
+//       page: 1,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Searchbar searching={this.searching} />
+
+//         {this.state.isLoading ? (
+//           <Loader />
+//         ) : (
+//           <ImageGallery items={this.state.images} />
+//         )}
+
+//         {this.state.images.length < this.state.totalPages && (
+//           <Button more={this.loadMore} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/Components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/Components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,45 +1,72 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from "react";
+import React, { useState } from "react";
 import { Component } from "react";
 import styles from "./ImageGalleryItem.module.css";
 import Modal from "../Modal/Modal";
 import PropTypes from "prop-types";
-export default class GalleryItem extends Component {
-  state = { isOpen: false };
 
-  modalToggle = (e) => {
-    e.stopPropagation();
-    this.setState({ isOpen: !this.state.isOpen });
+export default function GalleryItem(props) {
+  const [isOpen, setModal] = useState(false);
+function modalToggle (e){
+  e.stopPropagation();
+  setModal(!isOpen)
   };
-  handleKeyDown = (e) => {
+function handleKeyDown (e) {
     if (e.code === "Escape") {
-      this.setState({ isOpen: false });
+      setModal(false) 
     }
   };
-  render() {
-    const { webformatURL, largeImageURL, tags } = this.props;
-    window.addEventListener("keydown", this.handleKeyDown);
-    return (
+  const { webformatURL, largeImageURL, tags } = props;
+  window.addEventListener("keydown", handleKeyDown);
+  
+  return (
       <div>
-        <li className={styles.ImageGalleryItem} onClick={this.modalToggle}>
+        <li className={styles.ImageGalleryItem} onClick={modalToggle}>
           <img
             src={webformatURL}
             alt={tags}
             className={styles.ImageGalleryItem__image}
           />
         </li>
-        {this.state.isOpen && (
+        {isOpen && (
           <Modal
             largeImageURL={largeImageURL}
             tags={tags}
-            modalToggle={this.modalToggle}
+            modalToggle={modalToggle}
             
           />
         )}
       </div>
-    );
-  }
+    )
 }
+// export default class GalleryItem extends Component {
+  
+
+  
+  
+//   render() {
+   
+//     return (
+//       <div>
+//         <li className={styles.ImageGalleryItem} onClick={this.modalToggle}>
+//           <img
+//             src={webformatURL}
+//             alt={tags}
+//             className={styles.ImageGalleryItem__image}
+//           />
+//         </li>
+//         {this.state.isOpen && (
+//           <Modal
+//             largeImageURL={largeImageURL}
+//             tags={tags}
+//             modalToggle={this.modalToggle}
+            
+//           />
+//         )}
+//       </div>
+//     );
+//   }
+// }
 GalleryItem.propTypes = {
   webformatURL: PropTypes.string,
   largeImageURL: PropTypes.string,
